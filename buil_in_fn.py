8b3ee0a63e2984e3290b292469342a2bf5e5dfe5
MOHAMMAD AMAN KHAN <146350867+ynotaman@users.noreply.github.com>
2023-10-04 23:13:33 +0530

    Create buil_in_fn.py

---
+++ buil_in_fn.py
@@ -0,0 +1,24 @@
+lst1 = [1,2,3,4,5,6,7,8,9]
+lst2 =  ['aman', 'khan' , 'adnan' , 'anas', 'mine']
+
+print("to find the length of the list : ")      
+print(len(lst1))
+print(len(lst2))
+
+print("to find the max value of the list : ")
+print (max(lst1))
+print("to find the min value of the list : ")
+print(min(lst1))
+print("to find the sum of all value of the list : ")
+print(sum(lst1))
+print("to delete a given value  or slice the  list : ")
+# l=del(lst[2])
+# print (l))
+# print (del (lst1[1:3]))
+print("to reverse the list : ")
+print (reversed(lst1))
+a,b,c=10,20,30
+res=all((a>5,b>20,c>30))
+print(res)
+res=any((a>5,b>20,c>30))
+print(res)
